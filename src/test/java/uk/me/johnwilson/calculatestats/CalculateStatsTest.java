6cfc887d33d8604f158aece2beaba35a74fca863
jjwils <mail@johnwilson.me.uk>
2022-08-18 19:29:10 +0100

    start

---
+++ src/test/java/uk/me/johnwilson/calculatestats/CalculateStatsTest.java
@@ -0,0 +1,16 @@
+package uk.me.johnwilson.calculatestats;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculateStatsTest {
+
+    @Test
+    void testtest(){
+        assertEquals(1,1);
+    }
+
+
+
+}
